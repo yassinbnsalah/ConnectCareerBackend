ac4b28a23c854461b4db97b022e1e3cf586e38d3
yassinebnsalah <yacinbnsalh@gmail.com>
2024-02-04 17:30:05 +0100

    CreationOfRecruiter

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const admin = require('firebase-admin');
-const serviceAccount = require('./prv.json');
-const User = require('./models/user');
-const Entreprise = require('./models/entreprise');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const admin = require("firebase-admin");
+const serviceAccount = require("./prv.json");
+const User = require("./models/user");
+const Entreprise = require("./models/entreprise");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
-
+app.use(cors());
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "twinerz-fceb6.appspot.com",
 });
-
 
 // Middleware
 app.use(bodyParser.json());
@@ -23,62 +23,92 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(
   "mongodb+srv://yessinebensalah:14501578@connectcareerdb.3vb0ahj.mongodb.net/"
 );
-/*
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email: String,
-  imageUrl: String,
-});
 
-const User = mongoose.model("User", userSchema);*/
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-// Route pour l'inscription d'un utilisateur
 
-app.post("/inscriptionRecruiter", upload.fields([
-  { name: 'profileImage', maxCount: 1 },
-  { name: 'CompanyLogo', maxCount: 1 }
-]), async (req, res) => {
-  try{
-    const {firstname , lastname , email , phoneNumber , gender , password , 
-      jobTitle , jobType , CompanyName , CompanyAdress , CompanyCity , CompanyType ,
-    matriculeFiscale , description   } = req.body;
-    const role = "Recruiter"
-    let profileImage = "";
-    let CompanyLogo = "";
-   
-    if (req.files['profileImage']) {
-      const profileImageFile = req.files['profileImage'][0];
-      const imageExtension = profileImageFile.originalname.split('.').pop();
-      const imageName = `${firstname}${lastname}.${imageExtension}`;
-      
-      const profileImageBucket = admin.storage().bucket();
-      const profileImageFileObject = profileImageBucket.file(imageName);
-      await profileImageFileObject.createWriteStream().end(profileImageFile.buffer);
-      profileImage = `https://firebasestorage.googleapis.com/v0/b/${profileImageBucket.name}/o/${profileImageFileObject.name}`;
-    }
-    if (req.files['CompanyLogo']) {
-      const CompanyLogoFile = req.files['CompanyLogo'][0];
-      const CompanyLogoBucket = admin.storage().bucket();
-      const CompanyLogoFileObject = CompanyLogoBucket.file(CompanyLogoFile.originalname);
-      await CompanyLogoFileObject.createWriteStream().end(CompanyLogoFile.buffer);
-      CompanyLogo = `https://firebasestorage.googleapis.com/v0/b/${CompanyLogoBucket.name}/o/${CompanyLogoFileObject.name}`;
-    }
-    const entreprise = new Entreprise({CompanyName,CompanyAdress ,CompanyCity , CompanyType ,
-      matriculeFiscale ,  description ,  CompanyLogo})
-    await entreprise.save();
-    const newUser = new User({ firstname, lastname, email ,  phoneNumber ,gender ,
-      password , jobTitle ,jobType , role , profileImage , entreprise });
-    await newUser.save();
-    res.status(201).json({ message: "Utilisateur inscrit avec succès" });
-  }
-  catch(error){
-    console.error(error);
-  }
-})
+app.post(
+  "/inscriptionRecruiter",
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "CompanyLogo", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const {
+        firstname,
+        lastname,
+        email,
+        phoneNumber,
+        gender,
+        password,
+        jobTitle,
+        jobType,
+        CompanyName,
+        CompanyAdress,
+        CompanyCity,
+        CompanyType,
+        matriculeFiscale,
+        description,
+      } = req.body;
+      const role = "Recruiter";
+      let profileImage = "";
+      let CompanyLogo = "";
+      if (req.files["profileImage"]) {
+        const profileImageFile = req.files["profileImage"][0];
+        const imageExtension = profileImageFile.originalname.split(".").pop();
+        const imageName = `${firstname}${lastname}.${imageExtension}`;
 
-app.post("/inscription", upload.single('image'), async (req, res) => {
+        const profileImageBucket = admin.storage().bucket();
+        const profileImageFileObject = profileImageBucket.file(imageName);
+        await profileImageFileObject
+          .createWriteStream()
+          .end(profileImageFile.buffer);
+        profileImage = `https://firebasestorage.googleapis.com/v0/b/${profileImageBucket.name}/o/${profileImageFileObject.name}`;
+      }
+      if (req.files["CompanyLogo"]) {
+        const CompanyLogoFile = req.files["CompanyLogo"][0];
+        const CompanyLogoBucket = admin.storage().bucket();
+        const CompanyLogoFileObject = CompanyLogoBucket.file(
+          CompanyLogoFile.originalname
+        );
+        await CompanyLogoFileObject.createWriteStream().end(
+          CompanyLogoFile.buffer
+        );
+        CompanyLogo = `https://firebasestorage.googleapis.com/v0/b/${CompanyLogoBucket.name}/o/${CompanyLogoFileObject.name}`;
+      }
+      const entreprise = new Entreprise({
+        CompanyName,
+        CompanyAdress,
+        CompanyCity,
+        CompanyType,
+        matriculeFiscale,
+        description,
+        CompanyLogo,
+      });
+      await entreprise.save();
+      const newUser = new User({
+        firstname,
+        lastname,
+        email,
+        phoneNumber,
+        gender,
+        password,
+        jobTitle,
+        jobType,
+        role,
+        profileImage,
+        entreprise,
+      });
+      await newUser.save();
+      res.status(201).json({ message: "Utilisateur inscrit avec succès" });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
+//// DONT USE IT 
+app.post("/inscription", upload.single("image"), async (req, res) => {
   try {
     const { username, password, email } = req.body;
     console.log(username, password, email);
@@ -88,7 +118,7 @@ app.post("/inscription", upload.single('image'), async (req, res) => {
         .status(400)
         .json({ message: "Veuillez fournir tous les champs nécessaires." });
     }
-    let imageUrl = '';
+    let imageUrl = "";
     if (req.file) {
       // Upload image to Firebase Storage
       const bucket = admin.storage().bucket();
@@ -100,7 +130,7 @@ app.post("/inscription", upload.single('image'), async (req, res) => {
       imageUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${file.name}`;
     }
     // Création d'un nouvel utilisateur
-    const newUser = new User({ username, password, email ,  imageUrl});
+    const newUser = new User({ username, password, email, imageUrl });
 
     // Sauvegarde dans la base de données
     await newUser.save();
@@ -124,6 +154,8 @@ app.post("/inscription", upload.single('image'), async (req, res) => {
     }
   }
 });
+
+
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
