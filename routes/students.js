e5d668be64c4f6437b6f932e7085199f25176a5d
yassinebnsalah <yacinbnsalh@gmail.com>
2024-02-20 23:01:06 +0100

    Validation 20/02/2024

--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const StudentService = require('../services/studentService');
+const express = require("express");
+const StudentService = require("../services/studentService");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const multer = require("multer");
@@ -7,13 +7,13 @@ router.use(bodyParser.urlencoded({ extended: true }));
 const upload = multer();
 const admin = require("firebase-admin");
 router.get("/request", async (req, res) => {
-    try {
-        const students = await StudentService.getRequestListe();
-        res.json(students);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const students = await StudentService.getRequestListe();
+    res.json(students);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 router.get("/:studentId", async (req, res) => {
@@ -29,29 +29,34 @@ router.get("/:studentId", async (req, res) => {
   }
 });
 router.get("/", async (req, res) => {
-    try {
-        const students = await StudentService.getListStudents();
-        res.json(students);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const students = await StudentService.getListStudents();
+    res.json(students);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.post('/becomeAlumni/:studentId',   
-upload.fields([
-    { name: "diploma", maxCount: 1 }
-  ]),
-   async (req, res) => {
+router.post(
+  "/becomeAlumni/:studentId",
+  upload.fields([{ name: "diploma", maxCount: 1 }]),
+  async (req, res) => {
     try {
       const { studentId } = req.params;
-  
-      const updatedStudent = await StudentService.becomeAlumni(studentId, req,res, admin);
-  
+
+      const updatedStudent = await StudentService.becomeAlumni(
+        studentId,
+        req,
+        res,
+        admin
+      );
+
       res.json(updatedStudent);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
+      res.status(500).json({ message: "Internal Server Error" });
     }
-  });
+  }
+);
 module.exports = router;
