ad22d101e73f98a2df1ac235478dfa4224cff0b8
fedi-ben-Amor <Haelkyll@gmail.com>
2024-03-01 23:23:32 +0100

    update Password & getUserByEmail

--- routes/authentification.js
+++ routes/authentification.js
@@ -8,13 +8,28 @@ const {
   AuthentificationAdmin,
   ForgetPassword,
   ReceiveToken,
+  getUserByEmail,
+  UpdatePassword,
 } = require("../services/authentification");
 router.post("/login", async (req, res) => {
   await Authentification(req, res);
 });
-
+router.get("/getUserByEmail/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await getUserByEmail(email);
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 router.post("/loginadmin", async (req, res) => {
   await AuthentificationAdmin(req, res);
+});
+
+router.post("/UpdatePassword/:email", async (req, res) => {
+  await UpdatePassword(req, res);
 });
 
 router.post("/forgetpassword", async (req, res) => {
