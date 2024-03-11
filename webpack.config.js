8b99a81ca66baa69ca528a4d47e0ad935d6ceaef
yassinebnsalah <yacinbnsalh@gmail.com>
2024-03-11 22:44:10 +0100

    ReadyTOBuild

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js' // Output bundle name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply this rule to .js files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader', // Use babel-loader for transpilation
+          options: {
+            presets: ['@babel/preset-env'] // Use @babel/preset-env for ES6+ to ES5 conversion
+          }
+        }
+      }
+    ]
+  }
+  
+};
