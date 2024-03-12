04e4d5a23ff6d62813a9d2272a24c4a7b8632230
Wiem Ben Araar <wiem.benaraar@esprit.tn>
2024-03-12 03:58:12 +0100

    Sonar  code smells test15

--- services/recruiterService.js
+++ services/recruiterService.js
@@ -1,20 +1,16 @@
 const Entreprise = require("../models/entreprise");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const fs = require('fs');
+const fs = require("fs");
 const nodemailer = require("nodemailer");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 async function getListeRecruiter() {
   try {
-    return await User.find({ role: "Recruiter" }).populate(
-      "entreprise"
-    );
-
+    return await User.find({ role: "Recruiter" }).populate("entreprise");
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-    return res;
+    throw new Error("Internal Server Error");
   }
 }
 
@@ -43,7 +39,6 @@ async function createRecruiter(req, res, admin) {
       const profileImageFile = req.files["profileImage"][0];
       const imageExtension = profileImageFile.originalname.split(".").pop();
       const imageName = `${firstname}${lastname}.${imageExtension}`;
-
       const profileImageBucket = admin.storage().bucket();
       const profileImageFileObject = profileImageBucket.file(imageName);
       await profileImageFileObject
@@ -57,9 +52,9 @@ async function createRecruiter(req, res, admin) {
       const CompanyLogoFileObject = CompanyLogoBucket.file(
         CompanyLogoFile.originalname
       );
-      await CompanyLogoFileObject.createWriteStream().end(
-        CompanyLogoFile.buffer
-      );
+      await CompanyLogoFileObject
+        .createWriteStream()
+        .end(CompanyLogoFile.buffer);
       CompanyLogo = `https://firebasestorage.googleapis.com/v0/b/${CompanyLogoBucket.name}/o/${CompanyLogoFileObject.name}`;
     }
     const entreprise = new Entreprise({
@@ -89,27 +84,26 @@ async function createRecruiter(req, res, admin) {
       isVerify: false,
     });
     await newUser.save();
-
   } catch (error) {
     console.error(error);
+    throw new Error("Internal Server Error");
   }
 }
 
-async function verifyrecruiter(email) {
+async function verifyRecruiter(email) {
   try {
     const recruiter = await User.find({ email: email });
     console.log(recruiter);
-    if (recruiter.length != 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return recruiter.length !== 0;
   } catch (error) {
     console.error(error);
+    throw new Error("Internal Server Error");
   }
 }
+
 const secretKey = "qsdsqdqdssqds";
-async function sendMailtorecruiter(email,fullname) {
+
+async function sendMailToRecruiter(email, fullname) {
   const token = jwt.sign({ email }, secretKey, { expiresIn: "1d" });
   // create reusable transporter object using the default SMTP transport
   const htmlTemplate = fs.readFileSync(
@@ -126,7 +120,6 @@ async function sendMailtorecruiter(email,fullname) {
       pass: "ebrh bilu ygsn zrkw", // ethereal password
     },
   });
-
   const msg = {
     from: {
       name: "ConnectCareer Esprit",
@@ -134,27 +127,30 @@ async function sendMailtorecruiter(email,fullname) {
     },
     to: `${email}`,
     subject: "CONNECTCAREER Account Confirmation",
-    html: htmlTemplate.replace('{{username}}', fullname).replace('{{token}}', token),
+    html: htmlTemplate
+      .replace("{{username}}", fullname)
+      .replace("{{token}}", token),
   };
-  
-    try {
-      await transporter.sendMail(msg);
-      console.log("Email has been sent !");
-    } catch (error) {
-      console.error(error);
-    }
-  
-}
-async function getRecruiterDetails(recruiterId) {
-  try {
-   return await User.findById(recruiterId).populate("entreprise");
 
+  try {
+    await transporter.sendMail(msg);
+    console.log("Email has been sent !");
   } catch (error) {
     console.error(error);
     throw new Error("Internal Server Error");
   }
 }
-async function updateRecruiter(req,res,admin) {
+
+async function getRecruiterDetails(recruiterId) {
+  try {
+    return await User.findById(recruiterId).populate("entreprise");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Internal Server Error");
+  }
+}
+
+async function updateRecruiter(req, res, admin) {
   try {
     console.log(req.body);
     const updatedRecruiter = await User.findByIdAndUpdate(
@@ -162,34 +158,34 @@ async function updateRecruiter(req,res,admin) {
       { $set: req.body },
       { new: true }
     );
-    let profileImage ="" ; 
+    let profileImage = "";
     if (req.files["profileImage"]) {
       console.log("new Profile image");
       const profileImageFile = req.files["profileImage"][0];
       const imageExtension = profileImageFile.originalname.split(".").pop();
       const imageName = `${req.body.email}.${imageExtension}`;
-
       const profileImageBucket = admin.storage().bucket();
       const profileImageFileObject = profileImageBucket.file(imageName);
       await profileImageFileObject
         .createWriteStream()
         .end(profileImageFile.buffer);
       profileImage = `https://firebasestorage.googleapis.com/v0/b/${profileImageBucket.name}/o/${profileImageFileObject.name}`;
-      updatedRecruiter.profileImage = profileImage
-      await updatedRecruiter.save()
+      updatedRecruiter.profileImage = profileImage;
+      await updatedRecruiter.save();
     }
-   
- 
+
     return updatedRecruiter;
   } catch (error) {
     console.error(error);
     throw new Error("Internal Server Error");
   }
 }
+
 module.exports = {
   getListeRecruiter,
   createRecruiter,
-  verifyrecruiter,
+  verifyRecruiter,
   getRecruiterDetails,
   updateRecruiter,
+  sendMailToRecruiter,
 };
