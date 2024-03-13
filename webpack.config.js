d006eb0fe24ad9423d9f9b67f5b30e5b62dea62c
fedi-ben-Amor <Haelkyll@gmail.com>
2024-03-13 01:06:31 +0100

    test buid

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,52 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app.js', // Point d'entrée de votre application
+  entry: './app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Répertoire de sortie
-    filename: 'bundle.js' // Nom du bundle de sortie
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Appliquer cette règle aux fichiers .js
-        exclude: /node_modules/, // Exclure le répertoire node_modules
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Utiliser babel-loader pour la transpilation
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'] // Utiliser @babel/preset-env pour la conversion de ES6+ en ES5
+            presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader', // ou 'babel-loader' avec '@babel/preset-typescript'
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.d\.ts$/, // Règle pour les fichiers de définition TypeScript
+        use: [
+          {
+            loader: 'ts-loader', // Utiliser ts-loader pour gérer les fichiers .d.ts
+            options: {
+              // Ajoutez ici vos options ts-loader si nécessaire
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /main\.d\.ts$/,
+        use: 'ignore-loader'
       }
     ]
   },
@@ -25,8 +55,23 @@ module.exports = {
       "crypto": require.resolve("crypto-browserify"),
       "stream": require.resolve("stream-browserify"),
       "path": require.resolve("path-browserify"),
-      "fs": false,
-      "util": require.resolve("util/") // Ajout de fallback pour 'util'
+      "fs": false, // Exclure le module 'fs' car il est spécifique à Node.js
+      "zlib": require.resolve("browserify-zlib"),
+      "querystring": require.resolve("querystring-es3"),
+      "util": require.resolve("util/"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "http": require.resolve("stream-http"),
+      "module": false,
+      "vm": require.resolve("vm-browserify"),
+      "dns": false,
+      "net": false,
+      "tls": false,
+      "assert": false,
+      "child_process": false, // Exclure le module 'child_process'
+      "tty": false, // Exclure le module 'tty'
+      "constants": require.resolve("constants-browserify"),
+      "worker_threads": false
     }
   }
 };
