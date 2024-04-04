5b79c7fa9c53f1da87242602519b6d5ccc127a4f
fedi-ben-Amor <Haelkyll@gmail.com>
2024-04-04 21:01:20 +0100

    version final Devops

--- routes/alumni.js
+++ routes/alumni.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const alumniService = require('../services/alumniService');
-
 const router = express.Router();
-
-const admin = require('firebase-admin');
 const multer = require('multer');
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 const upload = multer();
+
 router.get('/', async (req, res) => {
   try {
     const alumnis = await alumniService.getListeAlumni();
-    res.json( alumnis );
+    res.json(alumnis);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 router.get('/:alumniId', async (req, res) => {
-    try {
-      const { alumniId } = req.params;
-  
-      const alumniDetails = await alumniService.getAlumniDetails(alumniId);
-  
-      res.json(alumniDetails);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    const { alumniId } = req.params;
+    const alumniDetails = await alumniService.getAlumniDetails(alumniId);
+    res.json(alumniDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 module.exports = router;
